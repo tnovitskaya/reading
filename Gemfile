f3fbd86f3d6bf48976ed33f37ed14e69ce84b0ea
Tatiana Novitskaya <tanyanvt@gmail.com>
2015-01-05 14:00:15 +0200

    make error messages pretty

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'devise'
 gem 'enumerize'
 gem 'bootstrap-sass'
 gem 'devise-i18n'
+gem 'russian', '~> 0.6.0'
 
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable, :confirmable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
