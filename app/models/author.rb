3a8cc9a9893321b294a2a17ca9be6be7b713699d
Bodigrim <andrew.lelechenko@gmail.com>
2015-01-07 00:30:27 +0200

    Add short_name to Author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,8 @@ class Author < ActiveRecord::Base
   has_many :book_and_author_links, dependent: :destroy
   has_many :books, through: :book_and_author_links
 
+  def short_name
+    "#{self.first_name.first}. #{self.last_name}"
+  end
+
 end
